30cdf99c6ffe2bbe997fb4ad4f9abdbabbafafd2
abulb0865-svg <abulb0865@gmail.com>
2026-09-09 14:01:11 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+require('./bot');
+
+mongoose.connect('mongodb://localhost:27017/ai-investment-app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
+
+app.use('/api', require('./routes/authRoutes'));
+app.use('/api', require('./routes/userRoutes'));
+app.use('/api/admin', require('./routes/adminRoutes'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
