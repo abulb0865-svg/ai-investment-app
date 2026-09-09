d3fa17553ec7b365bd35de99946d37ecd03e1c99
abulb0865-svg <abulb0865@gmail.com>
2026-09-10 00:10:11 +0600

    Update Deposit.js

--- models/Deposit.js
+++ models/Deposit.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const depositSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    userId: { type: String, required: true },
     amount: { type: Number, required: true },
-    binancePayId: { type: String, required: true },
+    usdAmount: { type: Number },
+    trxId: { type: String, required: true },
     status: { type: String, default: 'pending' },
     createdAt: { type: Date, default: Date.now }
 });
