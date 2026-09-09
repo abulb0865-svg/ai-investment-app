9b2cb19ab9c92f942d085f0ede4c5e97c0f2f3fa
abulb0865-svg <abulb0865@gmail.com>
2026-09-09 23:47:52 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
+
 const userSchema = new mongoose.Schema({
-    phone: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    telegramId: { type: String, unique: true, sparse: true }, // টেলিগ্রাম আইডির জন্য
+    userId: { type: String },
+    phone: { type: String, default: 'N/A' }, // required: true বাদ দেওয়া হলো
+    password: { type: String, default: 'telegram_user' }, // required: true বাদ দেওয়া হলো
     balance: { type: Number, default: 0 },
     referredBy: { type: String, default: null },
     totalTeamDeposit: { type: Number, default: 0 },
     lastCheckInDate: { type: String, default: '' },
     createdAt: { type: Date, default: Date.now }
 });
+
 module.exports = mongoose.model('User', userSchema);
